27ee9a8c0a0312b3b0eed0ed9eae634d582c1ab8
mario7746 <m.kennedykavouras@gmail.com>
2019-03-06 21:27:59 -0800

    Adds Room card variation in Card Component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Button from '../Button'
 
 import './style.css'
 
@@ -7,27 +8,71 @@ import './style.css'
 class Card extends Component {
 
   render() {
-    const { imgSrc, title, description } = this.props
-    return (
-      <div className="card-container">
-        <div className="image-container">
-          <img src={imgSrc} style={{height: 200, width: 300}}/>
-        </div>
-        <div>
-          <h3>{title}</h3>
+    const { imgSrc, title, subTitle, description, btnText, type, sqMeters, occupancy, price } = this.props
+
+    if(type === 'image-card') {
+      return (
+        <div className="card-container">
+          <div className="title-container">
+            <h3>{title}</h3>
+            <h5>{subTitle}</h5>
+          </div>
+          <div className="image-container">
+            <img src={imgSrc} style={{height: 200, width: 300}}/>
+          </div>
           <div className="text-container">
             <p>{description}</p>
           </div>
+          <div className="cta-container">
+            <Button label={btnText} size="small"/>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
+
+    if(type === 'room-card') {
+      return (
+        <div className="card-container">
+          <div className="title-container">
+            <h3>{title}</h3>
+            <h5>{subTitle}</h5>
+          </div>
+          <div className="image-container">
+            <img src={imgSrc} style={{height: 200, width: 300}}/>
+          </div>
+          <div className="info-container">
+            <ul className="room-info">
+              <li className="room-info__detail">
+                {sqMeters} Sq. Meters
+              </li>
+              <li className="room-info__detail">
+                {occupancy} Guests
+              </li>
+              <li className="room-info__detail">
+                €{price}
+              </li>
+            </ul>
+          </div>
+          <div className="cta-container">
+            <Button label="More Info" size="small"/>
+            <Button label="Book Now" size="small" variant="secondary"/>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
 Card.propTypes = {
+  type: PropTypes.string.isRequired,
   imgSrc: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
+  subTitle: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  btnText: PropTypes.string,
+  sqMeters: PropTypes.number,
+  occupancy: PropTypes.number,
+  price: PropTypes.number
 }
 
 export default Card
--- src/stories/index.js
+++ src/stories/index.js
@@ -10,9 +10,9 @@ import Card from '../components/Card'
 
 storiesOf('Button', module)
   .add('default', () => <Button label="Default Button" onClick={action('clicked')}/>)
-  .add('small', () => <Button label="Primary Button" variant='primary' size='small' onClick={action('clicked')}/>)
-  .add('medium', () => <Button label="Primary Button" variant='primary' size='medium' onClick={action('clicked')}/>)
-  .add('large', () => <Button label="Primary Button" variant='primary' size='large' onClick={action('clicked')}/>)
+  .add('small', () => <Button label="Small Button" variant='primary' size='small' onClick={action('clicked')}/>)
+  .add('medium', () => <Button label="Medium Button" variant='primary' size='medium' onClick={action('clicked')}/>)
+  .add('large', () => <Button label="Large Button" variant='primary' size='large' onClick={action('clicked')}/>)
   .add('secondary', () => <Button label="Secondary Button" variant='secondary' size='medium' onClick={action('clicked')}/>)
   .add('outlined', () => <Button label="Outlined Button" variant='outlined' size='medium' onClick={action('clicked')}/>);
 
@@ -20,4 +20,5 @@ storiesOf('Input', module)
   .add('default', () => <Input label='Default Input' onChange={action((e) => {`${e.target.value}`})}/>);
 
 storiesOf('Card', module)
-  .add('image card', () => <Card imgSrc='https://placehold.it/500X300' title="Example" description="Example text goes here"/>);
+  .add('image card', () => <Card type="image-card" imgSrc='https://placehold.it/500X300' title="Example" subTitle="Example Subtitle"description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, magnam? Quo beatae perferendis repudiandae, reprehenderit voluptates" btnText="CTA Text Here"/>)
+  .add('room card', () => <Card type="room-card" imgSrc='https://placehold.it/500X300' title="Example Room" subTitle="Example Subtitle" sqMeters={50} occupancy={2} price={350}/>);
